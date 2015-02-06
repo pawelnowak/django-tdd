3cbb23269b1daf2be77ce2a8c720dcbb53a693f0
Pawel Nowak <pawel@pawelnowak.com>
2015-02-06 10:47:21 +0100

    first unit test

--- contacts/contacts/urls.py
+++ contacts/contacts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'contacts.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('user_contacts.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- contacts/user_contacts/views.py
+++ contacts/user_contacts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, render
+from django.template import RequestContext
+from user_contacts.models import Phone, Person
+# from user_contacts.new_contact_form import ContactForm
 
-# Create your views here.
+def home(request):
+    return render_to_response('index.html')
