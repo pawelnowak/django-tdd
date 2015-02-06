1ed72f25eea70e6282c7415bde9357bc5151820f
Pawel Nowak <pawel@pawelnowak.com>
2015-02-06 10:14:13 +0100

    more admin tests

--- contacts/ft/tests.py
+++ contacts/ft/tests.py
@@ -5,6 +5,9 @@ from django.test import LiveServerTestCase
 
 class AdminTest(LiveServerTestCase):
 
+    # load fixtures
+    fixtures = ['fixtures/admin.json']
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -16,3 +19,18 @@ class AdminTest(LiveServerTestCase):
         self.browser.get(self.live_server_url + '/admin/')
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Django administration', body.text)
+        # users types in username and passwords and presses enter
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('admin')
+        password_field.send_keys(Keys.RETURN)
+        # login credentials are correct, and the user is redirected to the main admin page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+        # user clicks on the Users link
+        user_link = self.browser.find_elements_by_link_text('Users')
+        user_link[0].click()
+        # user verifies that user live@forever.com is present
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('live@forever.com', body.text)
